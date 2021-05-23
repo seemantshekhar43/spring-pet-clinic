e698419b002a259e0dc9668ebd04d0984e3a66d5
seemantshekhar43 <seemantshekhar43@gmail.com>
2021-05-23 17:45:45 +0530

    adding static resources. Closes #17.

--- pet-clinic-web/src/main/java/com/seemantshekhar/springpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/seemantshekhar/springpetclinic/controllers/VetController.java
@@ -17,6 +17,7 @@ public class VetController {
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets(Model model){
         model.addAttribute("vets", vetService.findAll());
+       String s = "dfs";
         return "vets/index";
     }
 }
